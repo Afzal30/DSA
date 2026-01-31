c454d472cba6a350856337dfb6d2bca13d4ac4d4
Afzal30 <56122813+Afzal30@users.noreply.github.com>
2026-01-31 20:17:36 +0530

    Create 744. Find Smallest Letter Greater Than Target.py

---
+++ Daily Challenge/744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        n = len(letters)
+        low = 0; high = n-1
+        min1 = "z" 
+        count = 0
+        while low<=high:
+            mid = (low+high)//2
+            # if ord(letters[mid]) > ord(target):
+            if letters[mid] > target:
+                min1 = min(letters[mid],min1)
+                high = mid - 1
+                count = 1
+            else:
+                low = mid + 1
+        if count == 0:
+            return letters[0]
+        else:
+            return min1
