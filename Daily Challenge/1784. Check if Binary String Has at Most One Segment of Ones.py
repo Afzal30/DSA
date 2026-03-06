a4d0654fbc45ed981255e8da1457fae60934c8c4
Afzal30 <56122813+Afzal30@users.noreply.github.com>
2026-03-06 11:22:50 +0530

    Create 1784. Check if Binary String Has at Most One Segment of Ones.py

---
+++ Daily Challenge/1784. Check if Binary String Has at Most One Segment of Ones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        #if s=='1':
+            #return True
+        if '01' in s:
+            return False
+        return True
+
+        
